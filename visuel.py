9138c41207ad8578c00f43a8c26a1e7c6028fdc7
Jean Alet <jean.alet@etu.iut-tlse3.fr>
2025-06-02 16:07:42 +0200

    jsp pk ça marche aps

--- visuel.py
+++ visuel.py
@@ -10,7 +10,7 @@ from scipy.stats import linregress
 
 # -----------------------------------------------------------------------------
 # 1. Connaissance des algorithmes de plus courts chemins
-# 1.1 Présentation de l’algorithme de Dijkstra
+# 1.1 Présentation de l'algorithme de  Dijkstra
 # -----------------------------------------------------------------------------
 
 # → Exemple à faire à la main (petite matrice) et insérer ici la matrice et appel
